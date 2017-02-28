708aa71ad8b36aae10912fb7b5df01c1da536b41
Faisal Muchsin Al Fikri <faisalmuchsin16@gmail.com>
2017-02-28 15:38:22 +0700

    Add files via upload

---
+++ Minggu Ke-2/Mahasiswa.java
@@ -0,0 +1,8 @@
+/*Program Minggu Ke-2*/
+class Mahasiswa{
+	public static void main(String[] args) {
+		System.out.println("Nama   :Faisal Muchsin Al Fikri\n");
+		System.out.println("NIM    :10115262\n");
+		System.out.println("Alamat :Majalengka");
+		}
+}
