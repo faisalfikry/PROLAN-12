dd507391a7948fe84a1a9ac5a86d8c4e28ee8df4
Faisal Muchsin Al Fikri <faisalmuchsin16@gmail.com>
2017-03-01 20:50:32 +0700

    Delete Belajarjava.java

--- Minggu Ke-2/Belajarjava.java
+++
@@ -1,7 +0,0 @@
-/*Program Minggu Ke-2*/
-class Belajarjava{
-	public static void main(String[] args) {
-		System.out.print("Belajar Java Yuks");
-		System.out.println("Belajar Java Yuks 1");
-		}
-}
