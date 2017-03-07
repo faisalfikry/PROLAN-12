278a531d8dc59e59287b5fec3d379e4cd8040d7c
Faisal Muchsin Al Fikri <faisalmuchsin16@gmail.com>
2017-03-07 22:28:32 +0700

    Add files via upload

---
+++ Minggu Ke-3/Supermarket/karyawanyogya.java
@@ -0,0 +1,22 @@
+class karyawanyogya
+{
+   public String Namakaryawan; 
+   String ttl, nomorID;
+   int umurkaryawan;
+   char Jeniskelamin;
+
+   void Melayani_pengunjung_yang_berbelanja()
+   {
+   }
+   void Mencatat_data_Barang()
+   {
+   }
+   void Memeriksa_barang_yang_sudah_tidak_layak()
+   {
+   }
+   void Membersihkan_ruangan_yang_kotor()
+   {
+          System.out.println("Saudara Faisal Muchsin Al Fikri, Anda Sudah Cukup Baik Menjadi Karyawan di Supermarket ini.");
+   }
+     
+}
