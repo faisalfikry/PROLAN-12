92ab20ddca62590e46e9e8b26056a3478c48531f
Faisal Muchsin Al Fikri <faisalmuchsin16@gmail.com>
2017-03-07 22:25:43 +0700

    Delete mainsupermarket.java

--- Minggu Ke-3/Supermarket/mainsupermarket.java
+++
@@ -1,9 +0,0 @@
-public class mainsupermarket
-{
-     public static void main(String[]args)
-	 {
-		 karyawanyogya faisal = new karyawanyogya();
-	     faisal.Namakaryawan = "Sodara Faisal Muchsin Al Fikri";
-		 faisal.Membersihkan_ruangan_yang_kotor();
-	 }
-}
