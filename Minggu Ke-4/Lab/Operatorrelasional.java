03d5f94b2fdaa781e8d36724d7b8a4d3234a0373
Faisal Muchsin Al Fikri <faisalmuchsin16@gmail.com>
2017-03-15 22:12:21 +0700

    Add files via upload

---
+++ Minggu Ke-4/Lab/Operatorrelasional.java
@@ -0,0 +1,22 @@
+class Operatorrelasional
+{
+public static void main(String[]args)
+	{
+		byte x =5;
+		byte y = 4;
+	System.out.println("Apakah x sama dengan Y? "+ (x==y));
+	System.out.println("Apakah X tidak sama dengan Y? "+(x!=y));
+	System.out.println("Apakah X  lebih besar dari Y? "+(x>y));
+	System.out.println("Apakah X kurang dari Y? "+(x<y));
+	System.out.println("Apakah X lebih besar dari atau sama dengan Y? "+(x >= y));
+	System.out.println("Apakah X kurang atau sama dengan Y? "+(x <= y));
+
+if (x != y)
+{
+	System.out.println("x tidak samadengan y");
+}else{
+	System.out.println("x samadengan y");
+		
+		}
+	}
+}
